670e8c42dfc6121e2cd0599b62b5e5729a98d16d
mominosin <s_shingo@icloud.com>
2023-04-05 23:26:59 +0900

    refacter

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -13,13 +13,19 @@ func Hello(name string, lang string) string {
 		name = "World"
 	}
 
-	if lang == spanish {
-		return spanishHelloPrefix + name
+	return greetingPrefix(lang) + name
+}
+
+func greetingPrefix(lang string) (prefix string) {
+	switch lang {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	if lang == french {
-		return frenchHelloPrefix + name
-	}
-	return englishHelloPrefix + name
+	return
 }
 
 func main() {

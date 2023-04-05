6c3eb526fc527603ef84b093bf8d3bcdf61e34c3
mominosin <s_shingo@icloud.com>
2023-04-05 23:18:48 +0900

    add heloworld & spanish

---
+++ hello_world/hello.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+const spanish = "Spanish"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+
+func Hello(name string, lang string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	if lang == spanish {
+		return spanishHelloPrefix + name
+	}
+	return englishHelloPrefix + name
+}
+
+func main() {
+	fmt.Println(Hello("world", "English"))
+}
